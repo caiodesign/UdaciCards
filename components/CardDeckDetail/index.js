92677e7e1c4f6f4f12508ac099883cd50fd7eac1
Caio Oliveira <caio.oliveira4@nextel.com.br>
2019-03-30 16:42:09 -0300

    added some styles

---
+++ components/CardDeckDetail/index.js
@@ -0,0 +1,107 @@
+/* eslint-disable react/prop-types */
+import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+import { CardContainer, Button, ButtonTitle, ButtonContainer, Title, SubTitle, HeaderContainer } from './styled'
+import { getCardDeckDetails, deleteDeck } from '../../actions'
+
+class CardDeckDetail extends PureComponent {
+
+  static navigationOptions = ({ navigation }) => navigation.state.params.navTitle
+
+  componentDidMount() {
+    const { getCardDeckDetails: getDeck, navigation } = this.props
+    getDeck(navigation.state.params.entryId)
+  }
+
+  componentDidUpdate() {
+    const { getCardDeckDetails: getDeck, navigation } = this.props
+    getDeck(navigation.state.params.entryId)
+  }
+
+  deleteItem() {
+    const { title, deleteDeck: delDeck, navigation } = this.props
+
+    delDeck(title)
+    navigation.goBack()
+  }
+
+  render() {
+    const { title, questions, navigation } = this.props
+
+    const styles = {
+      buttonStyle: {
+        borderRadius: 0,
+        marginLeft: 0,
+        marginRight: 0,
+        marginBottom: 0,
+      },
+    }
+
+
+    return (
+      <CardContainer>
+        <HeaderContainer>
+          <Title>{title}</Title>
+          <SubTitle>{questions ? questions.length : 0} cards</SubTitle>
+        </HeaderContainer>
+        <ButtonContainer>
+          <Button
+            color="lighter"
+            onPress={() => {
+              navigation.navigate(
+                'CardQuiz',
+                {
+                  navTitle: title,
+                  questions,
+                },
+              )
+            }}
+          >
+            <ButtonTitle color="dark">Play Quiz!</ButtonTitle>
+          </Button>
+        </ButtonContainer>
+        <ButtonContainer>
+          <Button
+            color="lighter"
+            onPress={() => {
+              navigation.navigate(
+                'AddCardQuestion',
+                {
+                  navTitle: title,
+                  title,
+                },
+              )
+            }}
+          >
+            <ButtonTitle color="dark">AddCard</ButtonTitle>
+          </Button>
+        </ButtonContainer>
+        <ButtonContainer>
+          <Button onPress={() => this.deleteItem()} color="dark">
+            <ButtonTitle color="lighter">DeleteDeck</ButtonTitle>
+          </Button>
+        </ButtonContainer>
+      </CardContainer>
+    )
+  }
+
+}
+
+const mapStateToProps = (state) => {
+
+  if (state.CardDeckDetail) {
+    const { title, questions } = state.CardDeckDetail
+
+    return { title, questions }
+  }
+
+}
+
+
+export default connect(
+  mapStateToProps,
+  {
+    deleteDeck,
+    getCardDeckDetails,
+  },
+)(CardDeckDetail)
