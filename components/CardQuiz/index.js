29a0cf19964c0793c129ecca22ad5f76fdd81d21
Caio Oliveira <caio.oliveira4@nextel.com.br>
2019-03-30 17:20:49 -0300

    added new styles

---
+++ components/CardQuiz/index.js
@@ -0,0 +1,164 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+
+import { Card } from 'react-native-elements'
+import { View, Text } from 'react-native'
+import { NavigationActions } from 'react-navigation'
+import { clearStorage, setLocalNotification } from '../../utils/helpers'
+import { CardContainer, Button, ButtonTitle, ButtonContainer, Title, SubTitle, HeaderContainer } from './styled'
+
+
+class CardQuiz extends React.Component {
+
+  static navigationOptions = ({ navigation }) => {
+    const title = navigation.state.params.navTitle
+
+    return { title }
+  }
+
+  state = {
+    showQuestion: true,
+    questions: this.getQuestions(),
+    currentQuestion: 0,
+    correctAnswers: 0,
+  }
+
+  getQuestions() {
+    const { navigation } = this.props
+    const { questions } = navigation.state.params
+
+    return questions
+  }
+
+  backToDeck() {
+    const { navigation } = this.props
+    const backAction = NavigationActions.back()
+    this.resetQuiz()
+    navigation.dispatch(backAction)
+    this.resetNotification()
+  }
+
+  resetQuiz() {
+    this.setState(() => {
+      return {
+        showQuestion: true,
+        questions: this.getQuestions(),
+        currentQuestion: 0,
+        correctAnswers: 0,
+      }
+    })
+    this.resetNotification()
+  }
+
+  resetNotification() {
+    clearStorage()
+      .then(setLocalNotification)
+  }
+
+  renderCard() {
+    const {
+      questions,
+      currentQuestion,
+      correctAnswers,
+      showQuestion,
+    } = this.state
+
+    if (currentQuestion < questions.length) {
+      return (
+        <CardContainer>
+          <HeaderContainer>
+            <Title>
+              {
+              showQuestion
+                ? `Q: ${questions[currentQuestion].question}`
+                : `A: ${questions[currentQuestion].answer}`
+              }
+            </Title>
+            <SubTitle>{`Question ${currentQuestion + 1} of ${questions.length}`}</SubTitle>
+          </HeaderContainer>
+
+          <ButtonContainer>
+            <Button
+              onPress={() => this.setState({ showQuestion: !showQuestion })}
+              color="lighter"
+            >
+              <ButtonTitle color="dark">
+                {showQuestion ? 'Answer' : 'Question'}
+              </ButtonTitle>
+            </Button>
+          </ButtonContainer>
+
+          <ButtonContainer>
+            <Button
+              onPress={() => {
+                this.setState({
+                  currentQuestion: currentQuestion + 1,
+                  correctAnswers: correctAnswers + 1,
+                })
+              }}
+              color="lighter"
+            >
+              <ButtonTitle>
+                Correct
+              </ButtonTitle>
+            </Button>
+          </ButtonContainer>
+
+
+          <ButtonContainer>
+            <Button
+              onPress={() => this.setState({ currentQuestion: currentQuestion + 1 })}
+              color="dark"
+            >
+              <ButtonTitle color="lighter">
+                Incorrect
+              </ButtonTitle>
+            </Button>
+          </ButtonContainer>
+
+        </CardContainer>
+      )
+    }
+
+    return (
+      <CardContainer>
+        <HeaderContainer>
+          <Title>
+            {`You got ${correctAnswers} out of ${questions.length}`}
+          </Title>
+        </HeaderContainer>
+        <ButtonContainer>
+          <Button onPress={() => this.resetQuiz()} color="lighter">
+            <ButtonTitle color="dark">
+              Start again
+            </ButtonTitle>
+          </Button>
+        </ButtonContainer>
+        <ButtonContainer>
+          <Button onPress={() => this.backToDeck()} color="dark">
+            <ButtonTitle color="lighter">
+              Back to Deck
+            </ButtonTitle>
+          </Button>
+        </ButtonContainer>
+      </CardContainer>
+    )
+  }
+
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignContent: 'center',
+        }}
+      >
+        {this.renderCard()}
+      </View>
+    )
+  }
+
+}
+
+export default CardQuiz
---
+++ components/CardQuiz/styled.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components/native'
+
+export const CardContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const HeaderContainer = styled(CardContainer)`
+  max-height: 200;
+`
+
+export const Title = styled.Text`
+  font-size: 20px;
+  color: #040F16;
+  font-weight: bold;
+`
+
+export const SubTitle = styled.Text`
+  font-size: 14px;
+  font-weight: lighter;
+  color: #040F16;
+  opacity: .7
+  margin-top: 5px;
+`
+
+export const Button = styled.TouchableOpacity`
+  background-color: ${({ color }) => (color === 'lighter') ? '#fff' : '#000'};
+  width: 300;
+  border: 1px solid #000;
+  padding: 10px;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+`
+
+export const ButtonTitle = styled.Text`
+  color: ${({ color }) => (color === 'lighter') ? '#fff' : '#000'};
+`
+
+export const ButtonContainer = styled.View`
+  max-height: 70;
+  flex: 1;
+  justify-content: center;
+  align-content: center;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`
